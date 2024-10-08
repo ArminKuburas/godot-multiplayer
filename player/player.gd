0c324070b076701f5e956277e693963789715e66
Armin Kuburas <49181652+ArminKuburas@users.noreply.github.com>
2024-10-08 08:00:27 +0300

    feat: basic player stuff is done now

--- player/player.gd
+++ player/player.gd
@@ -6,9 +6,19 @@ extends CharacterBody2D
 @export var max_jumps = 5
 
 @export var player_sprite: AnimatedSprite2D
+@export var player_camera: PackedScene
+@export var camera_height = 39
 
 @onready var initial_sprite_scale = player_sprite.scale
 var jump_count = 0
+var camera_instance
+
+
+func _ready():
+	set_up_camera()
+	
+func _process(_delta):
+	update_camera_pos()
 
 func _physics_process(_delta: float) -> void:
 	var horizontal_input = (
@@ -18,25 +28,19 @@ func _physics_process(_delta: float) -> void:
 	velocity.x = horizontal_input * movement_speed
 	velocity.y += gravity
 	
+	handle_movement_state()
+		
+	move_and_slide()
+	
+	face_movement_direction(horizontal_input)
+
+func handle_movement_state():
 	var is_falling = velocity.y > 0.0 and not is_on_floor()
 	var is_jumping = Input.is_action_just_pressed("jump") and is_on_floor()
 	var is_double_jumping = Input.is_action_just_pressed("jump") and not is_on_floor()
 	var is_jump_cancelled = Input.is_action_just_released("jump") and velocity.y < 0.0
 	var is_idle = is_zero_approx(velocity.x) and is_on_floor()
 	var is_walking = not is_zero_approx(velocity.x) and is_on_floor()
-	
-	if is_jumping and jump_count < max_jumps:
-		velocity.y = -jump_strength
-		jump_count += 1
-	elif is_double_jumping and jump_count < max_jumps:
-		velocity.y = -jump_strength
-		jump_count += 1
-	elif is_jump_cancelled:
-		velocity.y = 0.0
-	elif is_on_floor():
-		jump_count = 0
-		
-	move_and_slide()
 	
 	if is_jumping:
 		player_sprite.play("jump_start")
@@ -49,12 +53,31 @@ func _physics_process(_delta: float) -> void:
 	elif is_idle:
 		player_sprite.play("idle")
 	
+	if is_jumping and jump_count < max_jumps:
+		velocity.y = -jump_strength
+		jump_count += 1
+	elif is_double_jumping and jump_count < max_jumps:
+		velocity.y = -jump_strength
+		jump_count += 1
+	elif is_jump_cancelled:
+		velocity.y = 0.0
+	elif is_on_floor():
+		jump_count = 0
+
+func face_movement_direction(horizontal_input):
 	if not is_zero_approx(horizontal_input):
 		if horizontal_input < 0:
 			player_sprite.scale = Vector2(-initial_sprite_scale.x, initial_sprite_scale.y)
 		else:
 			player_sprite.scale = initial_sprite_scale
 
-
 func _on_animated_sprite_2d_animation_finished() -> void:
 	player_sprite.play("jump")
+
+func set_up_camera():
+	camera_instance = player_camera.instantiate()
+	camera_instance.global_position.y = camera_height
+	get_tree().current_scene.add_child.call_deferred(camera_instance)
+
+func update_camera_pos():
+	camera_instance.global_position.x = global_position.x
