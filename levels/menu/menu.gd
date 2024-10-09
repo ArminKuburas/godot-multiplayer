ffd51196605518bd0e7d885655ffd7c44952bc04
Armin Kuburas <49181652+ArminKuburas@users.noreply.github.com>
2024-10-09 05:52:56 +0300

    fix: tiles broke for some reason. Fixed some other minor things

--- levels/menu/menu.gd
+++ levels/menu/menu.gd
@@ -1,7 +1,12 @@
 extends Node
 
+@export var ui: Control
 @export var ip_line_edit: LineEdit
 @export var status_label: Label
+@export var level_container: Node
+@export var level_scene: PackedScene
+@export var not_connected_hbox: HBoxContainer
+@export var host_hbox: HBoxContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,19 +20,32 @@ func _process(_delta: float) -> void:
 
 
 func _on_host_button_pressed() -> void:
+	not_connected_hbox.hide()
+	host_hbox.show()
 	Lobby.create_game()
+	status_label.text = "Hosting!"
 
 
 func _on_join_button_pressed() -> void:
 	Lobby.join_game(ip_line_edit.text)
+	not_connected_hbox.hide()
 	status_label.text = "Connecting..."
 
 
 func _on_start_button_pressed() -> void:
-	pass # Replace with function body.
-	
+	hide_menu.rpc()
+	change_level.call_deferred(level_scene)
+
+func change_level(scene):
+	level_container.add_child(scene.instantiate())
+
 func _on_connection_failed():
 	status_label.text = "Failed to connect"
+	not_connected_hbox.show()
 	
 func _on_connected_to_server():
 	status_label.text = "Successfully connected!"
+
+@rpc("call_local", "authority", "reliable")
+func hide_menu():
+	ui.hide()
