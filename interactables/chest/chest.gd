f26b742b1b05e4364bdad120c08041e0a08a4a7c
Armin Kuburas <49181652+ArminKuburas@users.noreply.github.com>
2024-10-15 21:42:57 +0300

    feat: added chest

---
+++ interactables/chest/chest.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@export var chest_locked: Sprite2D
+@export var chest_unlocked: Sprite2D
+@export var is_locked = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _on_interactable_interacted():
+	if is_locked:
+		is_locked = false
+	set_chest_properties()
+
+func set_chest_properties():
+	chest_unlocked.visible = !is_locked
+	chest_locked.visible = is_locked
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_multiplayer_synchronizer_delta_synchronized() -> void:
+	set_chest_properties()
