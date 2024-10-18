d61339040a2b3f776db6594f77a022581a07cfed
Armin Kuburas <49181652+ArminKuburas@users.noreply.github.com>
2024-10-18 04:16:41 +0300

    feat: able to exit levels now and enter new ones

--- interactables/key_door/key_door.gd
+++ interactables/key_door/key_door.gd
@@ -1,8 +1,14 @@
 extends Node2D
+class_name KeyDoor
+
+signal all_players_finished
 
 @export var is_open = false
 @export var door_open: Sprite2D
 @export var door_closed: Sprite2D
+@export var exit_area: Area2D
+
+var finished_players = 0
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
@@ -11,6 +17,7 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 	if is_open:
 		return
 	is_open = true
+	exit_area.monitoring = true
 	area.get_owner().queue_free()
 	set_door_properties()
 
@@ -21,3 +28,10 @@ func set_door_properties():
 
 func _on_multiplayer_synchronizer_delta_synchronized() -> void:
 	set_door_properties()
+
+
+func _on_exit_area_body_entered(body: Node2D) -> void:
+	body.queue_free()
+	finished_players += 1
+	if finished_players > len(multiplayer.get_peers()):
+		all_players_finished.emit()
--- levels/level.gd
+++ levels/level.gd
@@ -1,24 +1,38 @@
 extends Node2D
 
+signal level_complete
+
 @export var players_container: Node2D
-@export var player_scene: PackedScene
+@export var player_scenes: Array[PackedScene]
 @export var spawn_points: Array[Node2D]
+@export var key_door: KeyDoor
 
 var next_spawn_point_index = 0
+var next_character_index = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if not multiplayer.is_server():
 		return
 	
-	multiplayer.peer_connected.connect(add_player)
 	multiplayer.peer_disconnected.connect(delete_player)
+	
 	for id in multiplayer.get_peers():
 		add_player(id)
+	
 	add_player(1)
+	
+	key_door.all_players_finished.connect(_on_all_players_finished)
+
+func _on_all_players_finished():
+	key_door.all_players_finished.disconnect(_on_all_players_finished)
+	level_complete.emit()
 
 func add_player(id):
-	var player_instance = player_scene.instantiate()
+	var player_instance = player_scenes[next_character_index].instantiate()
+	next_character_index += 1
+	if next_character_index >= len(player_scenes):
+		next_character_index = 0
 	player_instance.position = get_spawn_point()
 	player_instance.name = str(id)
 	players_container.add_child(player_instance)
